946e92da6eee9f0e4035c277a71aafcefd9c5ecf
anushan <anushan1508@gmail.com>
2023-03-18 09:59:53 +0530

    contact us done

--- src/pages/contactus/Contact.js
+++ src/pages/contactus/Contact.js
@@ -20,6 +20,7 @@ function Contact() {
                     title="Map"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193571.98123529268!2d-73.94604099999999!3d40.678178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sin!4v1573855593777!5m2!1sen!2sin">
                 </iframe>
+                </div>
                 <div className="contact-container">
                     <div className="contact-form-container">
                         <h2>Get in Touch</h2>
@@ -63,7 +64,6 @@ function Contact() {
                     </div>
                 </div>
             </div>
-        </div>
     );
 }
 
