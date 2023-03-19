025d2f27662da3d094cdf095e5d2aaec43046600
anushan <anushan1508@gmail.com>
2023-03-19 19:25:13 +0530

    aboutus section 1 done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Footer from "./components/footer/Footer";
 import Contact from "./pages/contactus/Contact";
 import Service from "./pages/service/Service";
+import AboutUs from "./pages/aboutus/AboutUs";
 
 
 function App() {
     return (
         <div className="App">
             {/*<Contact/>*/}
-            <Service/>
+            {/*<Service/>*/}
+            <AboutUs/>
             <Footer/>
         </div>
     );
---
+++ src/pages/aboutus/AboutUs.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import Hero from "../../components/Hero/Hero";
+import contactUsImg from "../../asserts/contact.jpg";
+import './AboutUs.css'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHome, faEnvelopeOpen} from "@fortawesome/free-solid-svg-icons";
+
+class AboutUs extends Component {
+    render() {
+        return (
+            <div className="about">
+                <Hero
+                    heading="About Us"
+                    background={contactUsImg}
+                />
+                <div className="two-column-section">
+                    <div className="image-column">
+                        <img src={contactUsImg} alt="Example Image"/>
+                    </div>
+                    <div className="content-column">
+                        <h2>Engineering Your Dreams With Us</h2>
+                        <p className="main-paragraph">
+                            Which cattle fruitful he fly visi won't let above lesser stars. Fly form wonder every let
+                            third form two air seas after us said day won light also together midst two female she great
+                            to open.
+                        </p>
+                        <div className="sub-section">
+                            <FontAwesomeIcon icon={faHome} className="icon-large"/>
+                            <h3>Certified Company</h3>
+                            <p>Be man air male shall under create light together grass fly dat also also his brought
+                                itself air abundantly</p>
+                        </div>
+                        <div className="sub-section">
+                            <FontAwesomeIcon icon={faEnvelopeOpen} className="icon-large"/>
+                            <h3>Experience Employee</h3>
+                            <p>Be man air male shall under create light together grass fly do also also his brought
+                                itself air abundantly</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default AboutUs;
